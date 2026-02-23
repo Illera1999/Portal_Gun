5309c05369363b9d7bbdcbe40ce5ea1754c371ca
Illera1999 <illerarodriguez1999@gmail.com>
2026-02-23 17:18:52 +0000

    Add extension for context

---
+++ lib/config/theme/extensions/context_bold_sizes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+extension ContextBoldSizes on BuildContext {
+  TextStyle get bold12 => TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
+  TextStyle get bold14 => TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+  TextStyle get bold16 => TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+  TextStyle get bold18 => TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+  TextStyle get bold40 => TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
+}
---
+++ lib/config/theme/extensions/context_font_sizes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+extension ContextFontSizes on BuildContext {
+  TextStyle get font12 => TextStyle(fontSize: 12);
+  TextStyle get font14 => TextStyle(fontSize: 14);
+  TextStyle get font16 => TextStyle(fontSize: 16);
+  TextStyle get font18 => TextStyle(fontSize: 18);
+  TextStyle get font24 => TextStyle(fontSize: 24);
+  TextStyle get font40 => TextStyle(fontSize: 40);
+}
